7ab6cf96ba380dc7f5558d81cf2404adfaf35e5d
etahoon <eslam.tahoon91@gmail.com>
2021-02-01 15:35:23 +0200

    create role

--- backend/src/resolvers/mutation/create-role.js
+++ backend/src/resolvers/mutation/create-role.js
@@ -1,104 +1,153 @@
 import { prisma } from '@';
 
-const createA = permissions => {
+const createPermissionRule = ({
+  organizationId,
+  branchId,
+  specialtyId,
+  userId,
+}) => {
   // permissions.map(p => {});
 };
 
 const createRole = async (_, { role }, { organizationId }) => {
   const { name, permissions } = role;
-  console.dir({ name, permissions });
 
-  const permissionsArgs = permissions.map(({ level, rules }) => ({
-    create: {
-      organization: {
-        connect: {
-          id: organizationId,
+  const permissionsArgs = permissions.map(({ level, all, actionId, rules }) =>
+    Object.assign(
+      {
+        organization: {
+          connect: {
+            id: organizationId,
+          },
         },
+        action: {
+          connect: {
+            id: actionId,
+          },
+        },
+        level: level,
       },
-      level: level,
-      rules: rules.map(({ branchId, specialtyId, userId }) => ({
-        create: Object.assign(
-          {
-            organization: {
-              connect: {
-                id: organizationId,
-              },
+      all
+        ? { all: true }
+        : {
+            rules: {
+              create: rules.map(({ branchId, specialtyId, userId }) =>
+                Object.assign(
+                  {
+                    organization: {
+                      connect: {
+                        id: organizationId,
+                      },
+                    },
+                  },
+                  branchId && {
+                    branch: {
+                      connect: {
+                        id: branchId,
+                      },
+                    },
+                  },
+                  specialtyId && {
+                    specialty: {
+                      connect: {
+                        id: specialtyId,
+                      },
+                    },
+                  },
+                  userId && {
+                    user: {
+                      connect: {
+                        id: userId,
+                      },
+                    },
+                  }
+                )
+              ),
             },
-          },
-          branchId && {
-            branch: {
-              connect: {
-                id: branchId,
-              },
-            },
-          },
-          specialtyId && {
-            specialty: {
-              connect: {
-                id: specialtyId,
-              },
-            },
-          },
-          userId && {
-            user: {
-              connect: {
-                id: userId,
-              },
-            },
+            // rules: rules.map(({ branchId, specialtyId, userId }) => ({
+            //   create: Object.assign(
+            //     {
+            //       organization: {
+            //         connect: {
+            //           id: organizationId,
+            //         },
+            //       },
+            //     },
+            //     branchId && {
+            //       branch: {
+            //         connect: {
+            //           id: branchId,
+            //         },
+            //       },
+            //     },
+            //     specialtyId && {
+            //       specialty: {
+            //         connect: {
+            //           id: specialtyId,
+            //         },
+            //       },
+            //     },
+            //     userId && {
+            //       user: {
+            //         connect: {
+            //           id: userId,
+            //         },
+            //       },
+            //     }
+            //   ),
+            // })),
           }
-        ),
-        // create: {
-        //   organization: {
-        //     connect: {
-        //       id: organizationId,
-        //     },
-        //   },
-        //   branch: {
-        //     connect: {
-        //       id: branchId,
-        //     },
-        //   },
-        //   specialty: {
-        //     connect: {
-        //       id: specialtyId,
-        //     },
-        //   },
-        //   user: {
-        //     connect: {
-        //       id: userId,
-        //     },
-        //   },
-        // },
-      })),
-    },
-  }));
+    )
+  );
+  // const permissionsArgs = permissions.map(({ level, all, rules }) => ({
+  //   create: {
+  //     organization: {
+  //       connect: {
+  //         id: organizationId,
+  //       },
+  //     },
+  //     level: level,
+  //     rules: rules.map(({ branchId, specialtyId, userId }) => ({
+  //       create: Object.assign(
+  //         {
+  //           organization: {
+  //             connect: {
+  //               id: organizationId,
+  //             },
+  //           },
+  //         },
+  //         branchId && {
+  //           branch: {
+  //             connect: {
+  //               id: branchId,
+  //             },
+  //           },
+  //         },
+  //         specialtyId && {
+  //           specialty: {
+  //             connect: {
+  //               id: specialtyId,
+  //             },
+  //           },
+  //         },
+  //         userId && {
+  //           user: {
+  //             connect: {
+  //               id: userId,
+  //             },
+  //           },
+  //         }
+  //       ),
+  //     })),
+  //   },
+  // }));
 
-  return prisma.role.create({
+  return prisma.permissionRole.create({
     data: {
       name,
-      permissions: permissionsArgs,
-      // permissions: {
-      //   create: {
-      //     organization: {
-      //       connect: {
-      //         id: organizationId,
-      //       },
-      //     },
-      //     level: 'Organization',
-      //     rules: {
-      //       create: {
-      //         organization: {
-      //           connect: {
-      //             id: organizationId,
-      //           },
-      //         },
-      //         branch: {
-      //           connect,
-      //         },
-      //       },
-      //     },
-      //   },
-      // },
+      permissions: {
+        create: permissionsArgs,
+      },
       organization: {
         connect: {
           id: organizationId,
--- frontend/src/components/permissions/role-permissions/index.js
+++ frontend/src/components/permissions/role-permissions/index.js
@@ -13,196 +13,10 @@ import {
 } from 'components';
 
 import PermissionRules from '../permission-rules';
-import useFetchAppointments from '../../../hooks/fetch-appointments';
 import usePermissions from 'hooks/use-permissions';
+import { ALL_CHOICE } from 'utils/constants';
 
-const data = {
-  '5ac3e9e0-cbbc-49ae-a6ae-e4fc499b5201': {
-    __typename: 'Action',
-    id: '5ac3e9e0-cbbc-49ae-a6ae-e4fc499b5201',
-    name: 'List',
-    subject: 'Appointment',
-    visibility: true,
-    level: 'Organization',
-    rules: [],
-  },
-  'b0a173c7-141d-4177-8a1a-509d4536a0f3': {
-    __typename: 'Action',
-    id: 'b0a173c7-141d-4177-8a1a-509d4536a0f3',
-    name: 'Create',
-    subject: 'Appointment',
-    visibility: true,
-    level: 'Branch',
-    rules: [
-      {
-        branchId: 1,
-      },
-    ],
-  },
-  '4a8e98f5-dc6d-4c43-8f25-7317f0cce11a': {
-    __typename: 'Action',
-    id: '4a8e98f5-dc6d-4c43-8f25-7317f0cce11a',
-    name: 'Reschedule',
-    subject: 'Appointment',
-    visibility: true,
-    level: 'Specialty',
-    rules: [
-      {
-        branchId: 1,
-        specialtyId: 1,
-      },
-    ],
-  },
-  '9a2c8e0b-eb3e-41cd-a07b-1aa6f0de6f9f': {
-    __typename: 'Action',
-    id: '9a2c8e0b-eb3e-41cd-a07b-1aa6f0de6f9f',
-    name: 'Finish',
-    subject: 'Appointment',
-    visibility: true,
-    level: 'User',
-    rules: [
-      {
-        branchId: 1,
-        userId: 1,
-      },
-    ],
-  },
-  'a5594daa-832c-4da5-af68-210579858966': {
-    __typename: 'Action',
-    id: 'a5594daa-832c-4da5-af68-210579858966',
-    name: 'Cancel',
-    subject: 'Appointment',
-    visibility: false,
-    level: null,
-    rules: [],
-  },
-  '6588eaf4-bc00-42a1-b60e-b226b59a093e': {
-    __typename: 'Action',
-    id: '6588eaf4-bc00-42a1-b60e-b226b59a093e',
-    name: 'Archive',
-    subject: 'Appointment',
-    visibility: false,
-    level: null,
-    rules: [],
-  },
-  'fa9cabfb-625b-434b-bf1a-2a076d6efc4a': {
-    __typename: 'Action',
-    id: 'fa9cabfb-625b-434b-bf1a-2a076d6efc4a',
-    name: 'View',
-    subject: 'Patient',
-    visibility: false,
-    level: null,
-    rules: [],
-  },
-  'a692d8e6-72bc-4341-9bf4-d4b7cffbbe4b': {
-    __typename: 'Action',
-    id: 'a692d8e6-72bc-4341-9bf4-d4b7cffbbe4b',
-    name: 'View',
-    subject: 'Accounting',
-    visibility: false,
-    level: null,
-    rules: [],
-  },
-  '5a644b8d-eeea-46ad-957d-c143f8731a98': {
-    __typename: 'Action',
-    id: '5a644b8d-eeea-46ad-957d-c143f8731a98',
-    name: 'Add Revenue',
-    subject: 'Accounting',
-    visibility: false,
-    level: null,
-    rules: [],
-  },
-  'bbb60d29-d29f-4f3b-a291-0af979f42848': {
-    __typename: 'Action',
-    id: 'bbb60d29-d29f-4f3b-a291-0af979f42848',
-    name: 'Add Expense',
-    subject: 'Accounting',
-    visibility: false,
-    level: null,
-    rules: [],
-  },
-  'bfb95762-3c28-40e3-9f5c-9fa23627ad09': {
-    __typename: 'Action',
-    id: 'bfb95762-3c28-40e3-9f5c-9fa23627ad09',
-    name: 'Edit Revenue',
-    subject: 'Accounting',
-    visibility: false,
-    level: null,
-    rules: [],
-  },
-  'e395fba8-c666-4282-b870-cf2aa2d36148': {
-    __typename: 'Action',
-    id: 'e395fba8-c666-4282-b870-cf2aa2d36148',
-    name: 'Edit Expense',
-    subject: 'Accounting',
-    visibility: false,
-    level: null,
-    rules: [],
-  },
-  '5b8e3303-a198-40cf-b87d-e61079873da4': {
-    __typename: 'Action',
-    id: '5b8e3303-a198-40cf-b87d-e61079873da4',
-    name: 'Print',
-    subject: 'Accounting',
-    visibility: false,
-    level: null,
-    rules: [],
-  },
-  'fb90df08-3b7a-4356-a983-eb152536f294': {
-    __typename: 'Action',
-    id: 'fb90df08-3b7a-4356-a983-eb152536f294',
-    name: 'View',
-    subject: 'Calendar',
-    visibility: false,
-    level: null,
-    rules: [],
-  },
-  'cf38ce84-a61f-4e69-b64b-c5b6bbee5ecb': {
-    __typename: 'Action',
-    id: 'cf38ce84-a61f-4e69-b64b-c5b6bbee5ecb',
-    name: 'Create Event',
-    subject: 'Calendar',
-    visibility: false,
-    level: null,
-    rules: [],
-  },
-  '549fc751-5006-4829-8676-4153efeba80c': {
-    __typename: 'Action',
-    id: '549fc751-5006-4829-8676-4153efeba80c',
-    name: 'View',
-    subject: 'Inventory',
-    visibility: false,
-    level: null,
-    rules: [],
-  },
-  '706b38cf-95ac-451c-ba05-80f34a2fdc4b': {
-    __typename: 'Action',
-    id: '706b38cf-95ac-451c-ba05-80f34a2fdc4b',
-    name: 'Add Item',
-    subject: 'Inventory',
-    visibility: false,
-    level: null,
-    rules: [],
-  },
-  'cbe0a364-8f4c-47c5-b0f4-145f60a492c1': {
-    __typename: 'Action',
-    id: 'cbe0a364-8f4c-47c5-b0f4-145f60a492c1',
-    name: 'View History',
-    subject: 'Inventory',
-    visibility: false,
-    level: null,
-    rules: [],
-  },
-  'd84c1d68-e757-4ebe-9655-23fc97355044': {
-    __typename: 'Action',
-    id: 'd84c1d68-e757-4ebe-9655-23fc97355044',
-    name: 'Define Item',
-    subject: 'Inventory',
-    visibility: false,
-    level: null,
-    rules: [],
-  },
-};
+import data from './test.json';
 
 const RolePermission = () => {
   const [ff, setFF] = useState({});
@@ -222,9 +36,9 @@ const RolePermission = () => {
     setRoleForm({ name: 'test role' });
   }, []);
 
-  // useEffect(() => {
-  //   setTimeout(() => setFF(data), 2000);
-  // }, []);
+  useEffect(() => {
+    setTimeout(() => setFF(data), 2000);
+  }, []);
 
   const toggle = useCallback(
     id => {
@@ -248,6 +62,8 @@ const RolePermission = () => {
       [actionId]: newActions,
     });
   };
+
+  // console.log(JSON.stringify(ff));
 
   const handleAddMapping = useCallback(
     (actionId, value) => {
@@ -283,14 +99,22 @@ const RolePermission = () => {
     [ff]
   );
 
-  const handleClick = useCallback(() => {
+  const handleCreateRole = useCallback(() => {
     const permissions = R.values(ff)
       .filter(ff => ff.visibility)
-      .map(({ id, rules, level }) => ({ actionId: id, rules, level }));
+      .map(({ id, rules, level }) =>
+        Object.assign(
+          { actionId: id, level },
+          rules.some(({ branchId }) => branchId === ALL_CHOICE)
+            ? { all: true }
+            : { rules }
+        )
+      );
     const role = {
       name: roleForm.name,
       permissions,
     };
+    console.log(role);
     createRole(role);
   }, [createRole, ff, roleForm.name]);
 
@@ -299,7 +123,7 @@ const RolePermission = () => {
       <MainContainer
         title="Role Permissions"
         more={
-          <CRButton small primary onClick={handleClick}>
+          <CRButton small primary onClick={handleCreateRole}>
             Save
           </CRButton>
         }
