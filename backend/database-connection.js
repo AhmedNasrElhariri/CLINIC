57eacfddebf5c73234f46a2c5ef833aeadeb6d81
etahoon <eslam.tahoon91@gmail.com>
2021-02-04 05:00:01 +0200

    fix add port

--- backend/database-connection.js
+++ backend/database-connection.js
@@ -2,8 +2,6 @@ const fs = require('fs');
 
 const fileName = process.env.ENV_FILE;
 
-console.log(fileName);
-
 const data = fs.readFileSync(fileName);
 
 fs.writeFileSync('./prisma/.env', data, function (err) {
--- backend/webpack.prod.js
+++ backend/webpack.prod.js
@@ -1,42 +1,51 @@
 const path = require('path');
+const webpack = require('webpack');
 const TerserPlugin = require('terser-webpack-plugin');
 const { merge } = require('webpack-merge');
 const CopyPlugin = require('copy-webpack-plugin');
 
 const common = require('./webpack.common.js');
 
-module.exports = merge(common, {
-  mode: 'production',
-  devtool: 'source-map',
-  optimization: {
-    minimizer: [new TerserPlugin()],
-  },
-  output: {
-    filename: 'main.js',
-    path: path.resolve(__dirname, 'dist'),
-  },
-  plugins: [
-    new CopyPlugin({
-      patterns: [
-        { from: 'node_modules/.prisma/client/schema.prisma', to: '' },
-        { from: 'node_modules/.prisma/client/index.js', to: '' },
-        {
-          from: 'node_modules/.prisma/client/query-engine-debian-openssl-1.1.x',
-          to: '',
-        },
-        {
-          from: '../frontend/build/',
-          to: 'build',
-        },
-        {
-          from: './prisma/migrations/',
-          to: 'migrations',
-        },
-        {
-          from: './.env',
-          to: '',
-        },
-      ],
-    }),
-  ],
-});
+module.exports = env => {
+  const envKeys = Object.keys(env || {}).reduce((prev, next) => {
+    prev[`process.env.${next}`] = JSON.stringify(env[next]);
+    return prev;
+  }, {});
+  return merge(common, {
+    mode: 'production',
+    devtool: 'source-map',
+    optimization: {
+      minimizer: [new TerserPlugin()],
+    },
+    output: {
+      filename: 'main.js',
+      path: path.resolve(__dirname, 'dist'),
+    },
+    plugins: [
+      new CopyPlugin({
+        patterns: [
+          { from: 'node_modules/.prisma/client/schema.prisma', to: '' },
+          { from: 'node_modules/.prisma/client/index.js', to: '' },
+          {
+            from:
+              'node_modules/.prisma/client/query-engine-debian-openssl-1.1.x',
+            to: '',
+          },
+          {
+            from: '../frontend/build/',
+            to: 'build',
+          },
+          {
+            from: './prisma/migrations/',
+            to: 'migrations',
+          },
+          {
+            from: './.env',
+            to: '',
+          },
+        ],
+      }),
+      new webpack.DefinePlugin(envKeys),
+    ],
+  });
+};
