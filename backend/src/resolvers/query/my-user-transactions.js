e373b531a6db08c49bb0f1c9633df7c0f1eca559
etahoon <eslam.tahoon91@gmail.com>
2021-04-13 23:18:50 +0200

    fix payroll name

--- backend/src/resolvers/query/my-user-transactions.js
+++ backend/src/resolvers/query/my-user-transactions.js
@@ -1,7 +1,6 @@
 import { prisma } from '@';
 
 const myUserTransactions = (_, { userId }) => {
-  console.log("skkkkkkkkkkkkkkkkkkkkkkkkkkkkkkskkkk",userId);
   return prisma.payrollTransaction.findMany({
     where: {
       payrollUserId: userId,
