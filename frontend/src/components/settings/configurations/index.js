2153a8f37aba553cec2ea591cf8edae3f56cb446
ahmed nasr <ahmednasr.fci97@gmail.com>
2021-07-02 17:50:29 +0200

    add

--- frontend/src/components/settings/configurations/index.js
+++ frontend/src/components/settings/configurations/index.js
@@ -1,28 +1,64 @@
 import React, { useState, useCallback, useEffect, useMemo } from 'react';
 import * as R from 'ramda';
-
-import { H3, Div, CRButton } from 'components';
-
+import * as moment from 'moment';
+import { Form } from 'rsuite';
+import { H3, Div, CRButton, CRNumberInput } from 'components';
+import SessionDefinitions from '../session-definations';
 import EnableInvoiceCounter from './enable-invoice-counter/index';
-import { useConfigurations } from 'hooks';
+import { useAuth, useConfigurations } from 'hooks';
+import { get } from './../../../services/local-storage';
+import { POSITIONS } from 'utils/constants';
 const initialValues = {
+  sessions: [],
   enableInvoiceCounter: false,
 };
+const initialPulsesValue = {
+  before: 0,
+  after: 0,
+};
+
 const Configurations = () => {
   const [formValue, setFormValue] = useState(initialValues);
-  const { configurations, update } = useConfigurations();
+  const [pulsesValue, setPulseValues] = useState(initialPulsesValue);
+  const { isOrAssistant } = useAuth();
+  const { configurations, update, addPulsesControl, getPulseControl } =
+    useConfigurations();
   useEffect(() => {
+    const sessions = R.pipe(
+      R.propOr([], 'sessions'),
+      R.map(R.pick(['name', 'price']))
+    )(configurations);
     const enableInvoiceCounter = R.propOr(
       false,
       'enableInvoiceCounter'
     )(configurations);
     setFormValue({
+      sessions,
       enableInvoiceCounter,
     });
-  }, [configurations]);
+    const before = R.propOr(0, 'before')(getPulseControl);
+    const after = R.propOr(0, 'after')(getPulseControl);
+    setPulseValues({ ...pulsesValue, before, after });
+  }, [configurations, getPulseControl]);
+  console.log(getPulseControl);
   const handleSave = useCallback(() => {
     update(formValue);
   }, [formValue, update]);
+
+  const sessions = useMemo(
+    () => R.propOr([], 'sessions')(formValue),
+    [formValue]
+  );
+
+  const updateSession = useCallback(
+    session => {
+      setFormValue({
+        ...formValue,
+        sessions: [...sessions, session],
+      });
+    },
+    [formValue, sessions]
+  );
 
   const updateEnable = useCallback(
     enable => {
@@ -33,6 +69,24 @@ const Configurations = () => {
     },
     [formValue]
   );
+  const handlePulsesSave = useCallback(() => {
+    addPulsesControl({
+      variables: {
+        pulsesControl: pulsesValue,
+      },
+    });
+  }, [pulsesValue, addPulsesControl]);
+
+  const handleDelete = useCallback(
+    idx => {
+      setFormValue({
+        ...formValue,
+        sessions: R.remove(idx, 1)(sessions),
+      });
+    },
+    [formValue, sessions]
+  );
+  const today = moment(new Date()).format('DD/MM/YYYY');
   return (
     <>
       <Div display="flex" justifyContent="space-between">
@@ -43,10 +97,50 @@ const Configurations = () => {
           </CRButton>
         </Div>
       </Div>
+      <SessionDefinitions
+        sessions={sessions}
+        onChange={updateSession}
+        onDelete={handleDelete}
+      />
       <EnableInvoiceCounter
         onChange={updateEnable}
         value={formValue?.enableInvoiceCounter}
       />
+      {isOrAssistant && (
+        <>
+          <hr></hr>
+          <Div display="flex" justifyContent="space-between">
+            <Div display="flex" justifyContent="space-around">
+              <H3 mb={64} mr={20}>
+                Pulses Control
+              </H3>
+              <H3>{today}</H3>
+            </Div>
+            <Div>
+              <CRButton onClick={handlePulsesSave} variant="primary">
+                Save
+              </CRButton>
+            </Div>
+          </Div>
+          <Form formValue={pulsesValue} onChange={setPulseValues}>
+            <Div display="flex" justifyContent="space-between">
+              <CRNumberInput
+                name="before"
+                label="before"
+                layout="inline"
+                disabled
+                placeholder="Pulses"
+              />
+              <CRNumberInput
+                name="after"
+                label="after"
+                layout="inline"
+                placeholder="Pulses"
+              />
+            </Div>
+          </Form>
+        </>
+      )}
     </>
   );
 };
