a9eb37e0540d1d2415cfd500b13e29465aa91867
ahmed nasr <ahmednasr.fci97@gmail.com>
2021-07-02 17:59:11 +0200

    add

--- frontend/src/hooks/configurations.js
+++ frontend/src/hooks/configurations.js
@@ -6,6 +6,8 @@ import { Alert } from 'rsuite';
 import {
   LIST_CONFIGURATIONS,
   UPDATE_CONFIGURATION,
+  ADD_PULSES_CONTROL,
+  GET_PULSE_CONTROL,
 } from 'apollo-client/queries';
 
 const useConfigurations = ({ onUpdate } = {}) => {
@@ -15,11 +17,26 @@ const useConfigurations = ({ onUpdate } = {}) => {
   const configurations = useMemo(() => R.propOr({}, 'configuration')(data), [
     data,
   ]);
+  const sessions = useMemo(() => R.propOr([], 'sessions')(configurations), [
+    configurations,
+  ]);
 
+  const { data: PulseData } = useQuery(GET_PULSE_CONTROL, {
+    fetchPolicy: 'network-only',
+  });
+  const getPulseControl = useMemo(
+    () => R.propOr({}, 'getPulseControl')(PulseData),
+    [PulseData]
+  );
   const [updateConfiguration] = useMutation(UPDATE_CONFIGURATION, {
     onCompleted: () => {
       Alert.success('Event has been updated successfully');
       onUpdate && onUpdate();
+    },
+  });
+  const [addPulsesControl] = useMutation(ADD_PULSES_CONTROL, {
+    onCompleted: () => {
+      Alert.success('Pulses Contol Added successfully');
     },
   });
   const handleUpdateConfiguration = useCallback(
@@ -30,11 +47,17 @@ const useConfigurations = ({ onUpdate } = {}) => {
   return useMemo(
     () => ({
       configurations,
+      sessions,
       update: handleUpdateConfiguration,
+      addPulsesControl,
+      getPulseControl,
     }),
     [
       configurations,
       handleUpdateConfiguration,
+      sessions,
+      addPulsesControl,
+      getPulseControl,
     ]
   );
 };
