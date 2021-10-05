e73e001630c554e05750f9b4f7b689e9cec87088
ahmed nasr <ahmednasr.fci97@gmail.com>
2021-10-05 12:40:22 +0200

    make the reference and the area not mandatory in the patient input

--- backend/src/services/get_Area.js
+++ backend/src/services/get_Area.js
@@ -1,5 +1,9 @@
 import { areas } from './areas';
 export const getArea = area => {
   const areaName = areas.find(e => e.id == area);
-  return areaName.city_name_en;
+  if (areaName) {
+    return areaName.city_name_en;
+  } else {
+    return '';
+  }
 };
