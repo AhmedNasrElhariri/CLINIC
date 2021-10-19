20a2db075002d9d888fe53856e9f0b407b8876b3
ahmed nasr <ahmednasr.fci97@gmail.com>
2021-10-19 15:31:45 +0200

    make the show medicines of all appointments of the patient feature

--- backend/src/resolvers/query/patient-appointments.js
+++ backend/src/resolvers/query/patient-appointments.js
@@ -1,9 +1,9 @@
 import { prisma } from '@';
 import { APPOINTMENTS_STATUS } from '@/utils/constants';
-const appointment = async (_, { patientId }) => {
+const appointment = async (_, { patientId, status }) => {
   return prisma.appointment.findMany({
     where: {
-      status: APPOINTMENTS_STATUS.SCHEDULED,
+      status: status,
       patientId: patientId,
     },
     orderBy: {
