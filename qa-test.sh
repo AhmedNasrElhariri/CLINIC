4ab12c98cdadc73444034a4c3b64539854db0710
etahoon <eslam.tahoon91@gmail.com>
2022-03-11 02:34:53 +0200

    rename pm2 service

--- qa-test.sh
+++ qa-test.sh
@@ -5,6 +5,7 @@ git pull
 
 echo 'start frontend build'
 cd ./frontend
+yarn install --frozen-lockfile
 yarn build
 echo 'frontend built successfullty'
 
@@ -13,6 +14,7 @@ cd ../backend
 echo "start backend build env yarn build"
 
 npx prisma generate
+yarn install --frozen-lockfile
 yarn build
 
 echo 'backend built successfullty'
