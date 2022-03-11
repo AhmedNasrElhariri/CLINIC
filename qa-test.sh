0b2bc082b2657bf94d96827cc3d3df802ebc2c0f
etahoon <eslam.tahoon91@gmail.com>
2022-03-11 02:26:54 +0200

    fix deploy

---
+++ qa-test.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+echo 'fetch latest'
+git pull 
+
+echo 'start frontend build'
+cd ./frontend
+yarn build
+echo 'frontend built successfullty'
+
+cd ../backend
+
+echo "start backend build env yarn build"
+
+npx prisma generate
+yarn build
+
+echo 'backend built successfullty'
+
+cp -Rf dist/. ../../root
+
+prisma migrate deploy
+pm2 stop clinicr1
+pm2 start ecosystem.config.js'
+
+echo 'starting ...'
+
