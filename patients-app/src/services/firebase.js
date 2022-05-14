c4bde96c23297af5a1e266cc3a6ec9c21987515b
etahoon <eslam.tahoon91@gmail.com>
2022-05-14 13:53:26 +0200

    change config

--- patients-app/src/services/firebase.js
+++ patients-app/src/services/firebase.js
@@ -1,36 +1,23 @@
-// import firebase from "firebase/app";
-// import { initializeApp } from "firebase/app";
-// import { getAuth, signInWithPhoneNumber } from "firebase/auth";
-// const firebaseConfig = {
-//   apiKey: "AIzaSyCAuChRD3mY_i2CD6Nv4qeFvf7e_iKIo-Y",
-//   authDomain: "clinicr-fcbed.firebaseapp.com",
-//   projectId: "clinicr-fcbed",
-//   storageBucket: "clinicr-fcbed.appspot.com",
-//   messagingSenderId: "147447029572",
-//   appId: "1:147447029572:web:0c01581633f98de7390111",
-// };
-// const app = initializeApp(firebaseConfig);
-// const auth = getAuth();
-// const appVerifier = app;
-// firebase.initializeApp(firebaseConfig);
-// // var auth = firebase.auth();
-// export { auth, appVerifier, signInWithPhoneNumber };
-
-// import firebase from "firebase/app";
-import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
+import { initializeApp } from 'firebase/app';
+import { getAuth } from 'firebase/auth';
 const firebaseConfig = {
-  apiKey: "AIzaSyCAuChRD3mY_i2CD6Nv4qeFvf7e_iKIo-Y",
-  authDomain: "clinicr-fcbed.firebaseapp.com",
-  projectId: "clinicr-fcbed",
-  storageBucket: "clinicr-fcbed.appspot.com",
-  messagingSenderId: "147447029572",
-  appId: "1:147447029572:web:0c01581633f98de7390111",
+  apiKey: 'AIzaSyBu5DPIIc_hEieLoO9kihOxyVAVJiQ2vH8',
+  authDomain: 'clinicr-a71ac.firebaseapp.com',
+  projectId: 'clinicr-a71ac',
+  storageBucket: 'clinicr-a71ac.appspot.com',
+  messagingSenderId: '295313587935',
+  appId: '1:295313587935:web:928c7cc215ded60cf3f36a',
+  measurementId: 'G-7PTZ4C3YVD',
 };
+// const firebaseConfig = {
+//   apiKey: 'AIzaSyCAuChRD3mY_i2CD6Nv4qeFvf7e_iKIo-Y',
+//   authDomain: 'clinicr-fcbed.firebaseapp.com',
+//   projectId: 'clinicr-fcbed',
+//   storageBucket: 'clinicr-fcbed.appspot.com',
+//   messagingSenderId: '147447029572',
+//   appId: '1:147447029572:web:0c01581633f98de7390111',
+// };
 
-// firebase.initializeApp(firebaseConfig);
-// var auth = firebase.auth();
-// export { auth, firebase };
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 export { auth, app };
