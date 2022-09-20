8c3ef8b0ac82374382435825fe0a5b1220920f9c
Mostafa Ellethy <mostafa.ellethy93@gmail.com>
2022-09-20 18:51:35 +0200

    filter responsive

--- frontend/src/components/appointments/appointments/filter.js
+++ frontend/src/components/appointments/appointments/filter.js
@@ -1,49 +1,54 @@
 import React from 'react';
-import { Form, Row, Col } from 'rsuite';
+import { Form } from 'rsuite';
 import { useTranslation } from 'react-i18next';
 import { appointmentTypes, appointmentStatus } from 'services/appointment';
 import { CRTextInput, CRSelectInput, CRDateRangePicker } from 'components';
 
-function AppointmentsFilter({ formValue, onChange }) {
+const FilterContainer = ({ children }) => (
+  <div>{children}</div>
+);
+
+export default function AppointmentsFilter({ formValue, onChange }) {
   const { t } = useTranslation();
   return (
-    <Form formValue={formValue} onChange={onChange} fluid>
-      <Row gutter={16}>
-        <Col xs={6}>
-          <CRSelectInput
-            name="status"
-            label={t('status')}
-            block
-            data={appointmentStatus}
-            onChange={val =>
-              val == null
-                ? onChange({ ...formValue, status: 'Scheduled' })
-                : onChange({ ...formValue, status: val })
-            }
-          />
-        </Col>
-        <Col xs={6}>
-          <CRDateRangePicker
-            name="date"
-            label={t('range')}
-            placeholder={t('timeframe')}
-            block
-          />
-        </Col>
-        <Col xs={6}>
-          <CRTextInput name="patient" label={t('nameOrPhoneNo')} />
-        </Col>
-        <Col xs={6}>
-          <CRSelectInput
-            name="type"
-            label={t('type')}
-            block
-            data={appointmentTypes}
-          />
-        </Col>
-      </Row>
+    <Form
+      formValue={formValue}
+      onChange={onChange}
+      fluid
+      className="grid grid-cols-2 gap-4 sm:grid-cols-3 md:grid-cols-4"
+    >
+      <FilterContainer>
+        <CRSelectInput
+          name="status"
+          label={t('status')}
+          block
+          data={appointmentStatus}
+          onChange={val =>
+            val == null
+              ? onChange({ ...formValue, status: 'Scheduled' })
+              : onChange({ ...formValue, status: val })
+          }
+        />
+      </FilterContainer>
+      <FilterContainer>
+        <CRDateRangePicker
+          name="date"
+          label={t('range')}
+          placeholder={t('timeframe')}
+          block
+        />
+      </FilterContainer>
+      <FilterContainer>
+        <CRTextInput name="patient" label={t('nameOrPhoneNo')} />
+      </FilterContainer>
+      <FilterContainer>
+        <CRSelectInput
+          name="type"
+          label={t('type')}
+          block
+          data={appointmentTypes}
+        />
+      </FilterContainer>
     </Form>
   );
 }
-
-export default AppointmentsFilter;
