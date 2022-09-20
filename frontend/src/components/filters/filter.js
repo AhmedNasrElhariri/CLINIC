56dd449605989bcd5afec26d6e75ffc2adf39f40
Mostafa Ellethy <mostafa.ellethy93@gmail.com>
2022-09-20 18:48:51 +0200

    branch filter responsive

--- frontend/src/components/filters/filter.js
+++ frontend/src/components/filters/filter.js
@@ -1,14 +1,15 @@
 import React, { useEffect, useMemo } from 'react';
-import { Form, Row, Col } from 'rsuite';
+import { Form } from 'rsuite';
 import { get } from 'services/local-storage';
-import { CRSelectInput, Div } from 'components';
+import { CRSelectInput } from 'components';
 import { useTranslation } from 'react-i18next';
 import * as R from 'ramda';
 function AppointmentsFilter({ formValue, onChange, branches }) {
+  const branch = get('branch');
   const { t } = useTranslation();
   useEffect(() => {
-    onChange({ ...formValue, branch: get('branch') });
-  }, [get('branch')]);
+    onChange({ ...formValue, branch: branch });
+  }, [branch]);
   const specialties = useMemo(
     () =>
       R.pipe(
@@ -27,45 +28,50 @@ function AppointmentsFilter({ formValue, onChange, branches }) {
     [formValue.specialty, specialties]
   );
   return (
-    <Form formValue={formValue} onChange={onChange} fluid>
-      <Div display="flex" width="100%" justifyContent="space-between">
-        {/* <Row gutter={16} > */}
-        {/* <Col xs={8}> */}
-        <Div width="32%">
-          <CRSelectInput
-            name="branch"
-            label={t('branch')}
-            placeholder={t('select')}
-            data={branches}
-            block
-          />
-        </Div>
-        {/* </Col> */}
-        {/* <Col xs={8}> */}
-        <Div width="32%">
-          <CRSelectInput
-            name="specialty"
-            label={t('specialty')}
-            placeholder={t('select')}
-            block
-            data={specialties}
-          />
-        </Div>
-        {/* </Col> */}
+    <Form
+      formValue={formValue}
+      onChange={onChange}
+      fluid
+      className="grid grid-cols-2 sm:grid-cols-3 gap-4"
+    >
+      <div className="flex-1">
+        <CRSelectInput
+          name="branch"
+          label={t('branch')}
+          placeholder={t('select')}
+          data={branches}
+          block
+        />
+      </div>
+      <div className="flex-1">
+        <CRSelectInput
+          name="specialty"
+          label={t('specialty')}
+          placeholder={t('select')}
+          block
+          data={specialties}
+        />
+      </div>
+      <div className="flex-1">
+        <CRSelectInput
+          name="doctor"
+          label={t('user')}
+          block
+          data={doctors}
+          placeholder={t('select')}
+        />
+      </div>
+      {/* <Div display="flex" width="100%" justifyContent="space-between"> */}
+      {/* <Div width="32%"> */}
 
-        {/* <Col xs={8}> */}
-        <Div width="32%">
-          <CRSelectInput
-            name="doctor"
-            label={t('user')}
-            block
-            data={doctors}
-            placeholder={t('select')}
-          />
-        </Div>
-        {/* </Col> */}
-        {/* </Row> */}
-      </Div>
+      {/* </Div> */}
+      {/* <Div width="32%"> */}
+
+      {/* </Div> */}
+      {/* <Div width="32%"> */}
+
+      {/* </Div> */}
+      {/* </Div> */}
     </Form>
   );
 }
