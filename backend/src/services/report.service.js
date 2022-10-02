648f1afdcb0e6c7a34e00247cceff74f9fe4aa0f
Ahmed Nasr <ahmednasr.fci97@gmail.com>
2022-10-02 17:35:52 +0200

    complete

--- backend/src/services/report.service.js
+++ backend/src/services/report.service.js
@@ -1,5 +1,5 @@
 import ejs from 'ejs';
-import { promises as fsp } from 'fs';
+import fs from 'fs';
 import pdf from 'html-pdf';
 const posthtml = require('posthtml');
 const postcss = require('posthtml-postcss');
@@ -11,19 +11,20 @@ const postcssOptions = {};
 const options = { format: 'A4' };
 
 export const generatePdf = async (path, vairables = {}) => {
-  const file = await fsp.readFile(__dirname + path, 'utf8');
+  const file = await fs.readFileSync(__dirname + path, 'utf8');
   const compiled = ejs.compile(file);
 
   const html = await posthtml([
     postcss(postcssPlugins, postcssOptions, filterType),
   ])
     .process(compiled(vairables))
-    .then(result => result.html);
+    .then(result => result.html).catch(err => console.log(err,'htmlERROR'));
   console.log(html,'html');
   return new Promise((resolve, reject) => {
-    pdf.create(html, options).toBuffer((err, res) => {
+    pdf.create(html).toBuffer((err, res) => {
       if (err) {
         reject(err);
+        console.log(err,'ERR');
       }
       resolve(res);
     });
