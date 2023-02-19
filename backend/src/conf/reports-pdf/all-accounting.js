648c178ba4baf809c214338b47c94df93f7802ef
etahoon <eslam@tahoon91@gmail.com>
2023-02-19 12:14:43 +0200

    fix acounting

--- backend/src/conf/reports-pdf/all-accounting.js
+++ backend/src/conf/reports-pdf/all-accounting.js
@@ -7,6 +7,10 @@ import {
   getStartOfDay,
   getEndOfDay,
 } from '@/services/date.service';
+
+const mapTypes = (transactions = [], type) =>
+  transactions.map(transaction => ({ ...transaction, type }));
+
 const init = app => {
   app.post('/allAccounting', async (req, res) => {
     const {
@@ -22,12 +26,13 @@ const init = app => {
       organizationId,
     } = req.query;
     try {
+      const transactions = [];
+      let totalAmount = 0;
       let updatedDateFrom = new Date();
       let updatedDateTo = new Date();
-      let trans = [];
-      let total = 0;
       const dateFrom = period && period[0];
       const dateTo = period && period[1];
+
       if (dateFrom && dateTo) {
         updatedDateFrom = getStartOfDay(dateFrom);
         updatedDateTo = getEndOfDay(dateTo);
@@ -36,8 +41,20 @@ const init = app => {
         updatedDateFrom = datesArray[0];
         updatedDateTo = datesArray[1];
       }
-      if (transactionType === 'revenue') {
-        const revenues = await prisma.revenue.findMany({
+
+      const accountingOptions = accountingOption
+        ? [accountingOption]
+        : ['cash', 'visa'];
+
+      const accountingOptionsVsModel = {
+        cash: { revene: prisma.revenue, expense: prisma.expense },
+        visa: { revene: prisma.bankRevenue, expense: prisma.bankExpense },
+      };
+
+      if (accountingOptions.includes('cash')) {
+        const revenues = await accountingOptionsVsModel['cash'][
+          transactionType
+        ].findMany({
           where: {
             organizationId,
             AND: [
@@ -61,134 +78,48 @@ const init = app => {
             },
           },
         });
-        const bankRevenues = await prisma.bankRevenue.findMany({
-          where: {
-            organizationId,
-            AND: [
-              {
-                branchId: branchId,
-              },
-              {
-                specialtyId: specialtyId,
-              },
-              {
-                doctorId: doctorId,
-              },
-              {
-                bankId: bankId,
-              },
-            ],
-            date: {
-              gte: updatedDateFrom,
-              lte: updatedDateTo,
-            },
-            name: {
-              contains: name,
-              mode: 'insensitive',
-            },
-          },
-        });
-        const updatedRevenues = revenues.map(r => {
-          return { ...r, date: formatDateStandard(r.date), flag: 'cash' };
-        });
-        const updatedBankRevenues = bankRevenues.map(r => {
-          return { ...r, date: formatDateStandard(r.date), flag: 'visa' };
-        });
-        const totalBankRevenue = updatedBankRevenues.reduce(
-          (acc, e) => acc + e.amount,
-          0
-        );
-        const totalRevenues = revenues.reduce((acc, e) => acc + e.amount, 0);
-        if (accountingOption === 'cash') {
-          trans = updatedRevenues;
-          total = totalRevenues;
-        } else if (accountingOption === 'visa') {
-          trans = updatedBankRevenues;
-          total = totalBankRevenue;
-        } else {
-          trans = [...updatedRevenues, ...updatedBankRevenues];
-          total = totalRevenues + totalBankRevenue;
-        }
-      } else {
-        const expenses = await prisma.expense.findMany({
-          where: {
-            organizationId,
-            AND: [
-              {
-                branchId: branchId,
-              },
-              {
-                specialtyId: specialtyId,
-              },
-              {
-                doctorId: doctorId,
-              },
-            ],
-            date: {
-              gte: updatedDateFrom,
-              lte: updatedDateTo,
-            },
-            name: {
-              contains: name,
-              mode: 'insensitive',
-            },
-          },
-        });
-        const bankExpenses = await prisma.bankExpense.findMany({
-          where: {
-            organizationId,
-            AND: [
-              {
-                branchId: branchId,
-              },
-              {
-                specialtyId: specialtyId,
-              },
-              {
-                doctorId: doctorId,
-              },
-              {
-                bankId: bankId,
-              },
-            ],
-            date: {
-              gte: updatedDateFrom,
-              lte: updatedDateTo,
-            },
-            name: {
-              contains: name,
-              mode: 'insensitive',
-            },
-          },
-        });
-        const updatedExpenses = expenses.map(r => {
-          return { ...r, date: formatDateStandard(r.date), flag: 'cash' };
-        });
-        const updatedBankExpeses = bankExpenses.map(r => {
-          return { ...r, date: formatDateStandard(r.date), flag: 'visa' };
-        });
-        const totalBankExpenses = updatedBankExpeses.reduce(
-          (acc, e) => acc + e.amount,
-          0
-        );
-        const totalExpenses = updatedExpenses.reduce(
-          (acc, e) => acc + e.amount,
-          0
-        );
-        if (accountingOption === 'cash') {
-          trans = updatedExpenses;
-          total = totalExpenses;
-        } else if (accountingOption === 'visa') {
-          trans = updatedBankExpeses;
-          total = totalBankExpenses;
-        } else {
-          trans = [...updatedExpenses, ...updatedBankExpeses];
-          total = totalExpenses + totalBankExpenses;
-        }
+        transactions.push(...mapTypes(revenues, 'cash'));
       }
+
+      if (accountingOptions.includes('visa')) {
+        const bankRevenues = await accountingOptionsVsModel['visa'][
+          transactionType
+        ].findMany({
+          where: {
+            organizationId,
+            AND: [
+              {
+                branchId: branchId,
+              },
+              {
+                specialtyId: specialtyId,
+              },
+              {
+                doctorId: doctorId,
+              },
+              {
+                bankId: bankId,
+              },
+            ],
+            date: {
+              gte: updatedDateFrom,
+              lte: updatedDateTo,
+            },
+            name: {
+              contains: name,
+              mode: 'insensitive',
+            },
+          },
+        });
+        transactions.push(...mapTypes(bankRevenues, 'visa'));
+      }
+
+      totalAmount = transactions.reduce((acc, { amount }) => acc + amount, 0);
+      transactions.sort((a, b) => b.date - a.date);
+
       const pdfDoc = await generatePdf('/views/reports/all-accounting.ejs', {
-        trans,
-        total,
+        transactions,
+        total: totalAmount,
         from: formatDateStandard(updatedDateFrom),
         to: formatDateStandard(updatedDateTo),
       });
