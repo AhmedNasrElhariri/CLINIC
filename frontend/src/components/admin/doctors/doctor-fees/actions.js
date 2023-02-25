bb8df4185d874600a0ef443a4f88ce5219232d94
Ahmed Nasr <ahmednasr.fci97@gmail.com>
2023-02-26 00:48:14 +0200

    complete

--- frontend/src/components/admin/doctors/doctor-fees/actions.js
+++ frontend/src/components/admin/doctors/doctor-fees/actions.js
@@ -39,7 +39,6 @@ const Actions = ({
         <CRButton onClick={addNewFees}>Add new fees </CRButton>
       </Div>
       <Div mt="43px" ml="10px">
-<<<<<<< HEAD
         <Whisper
           placement="bottomStart"
           trigger="click"
@@ -47,9 +46,6 @@ const Actions = ({
         >
           <CRButton>Prints</CRButton>
         </Whisper>
-=======
-        <CRButton onClick={print}>Print </CRButton>
->>>>>>> 2b97e55f99e6c513c84615ebb612c5f3dbb903ef
       </Div>
     </>
   );
