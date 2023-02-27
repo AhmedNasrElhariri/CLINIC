977b440a4e0fb9dedfeb3a8efbe832078b496b1b
etahoon <eslam@tahoon91@gmail.com>
2023-02-28 01:45:04 +0200

    fix build script

--- qa-build-pm4.sh
+++ qa-build-pm4.sh
@@ -50,15 +50,15 @@ zip -r ../build.zip *
 cd ../
 rsync -azP build.zip root@164.90.178.150:~/clinicr
 
-ssh root@164.90.178.150 "cd clinicr &&
+ssh root@164.90.178.150 'cd clinicr &&
  unzip -o ./build.zip -d . && rm build.zip && prisma migrate deploy && \
  pm2 delete clinicr || true && pm2 delete patients_app || true && \
  npm install && npx prisma generate && npx webpack --mode='production' --config ./webpack.dev.js && \
  cp -r ./frontend ./server-dist/frontend
-"
-ssh root@164.90.178.150 "cd clinicr &&
+'
+ssh root@164.90.178.150 'cd clinicr &&
  pm2 start ecosystem.config.js && pm2 serve patients-app/ 5000 --name "patients_app" --spa
-"
+'
 
 echo 'starting ...'
 
